be196f6002a836df2f85e8f12c5670247f6a504a
arianhassani <arianhassaniprof@gmail.com>
2025-07-21 23:04:18 +1000

    j

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -13,7 +13,7 @@ export default function Homepage() {
       <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
       <p className="mb-5">
         Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
+        quasi. In deleniti eaque aut repudiandae et a id nsi.
       </p>
       <button className="btn btn-primary">Get Started</button>
     </div>
